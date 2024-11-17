35b850a09974c910a9ed2dd606f126955b33034b
Poorva Ahuja <122693422+Poorvaahuja@users.noreply.github.com>
2024-11-17 17:00:06 +0530

    Delete lib/consts.dart

--- lib/consts.dart
+++
@@ -1,4 +0,0 @@
-const OPENWEATHER_API_KEY = "fc1c90e712cb86c5b9178496b1ecb1a3";
-const String NEWS_API_KEY = "ba619315eb474b7b8193f9ab28c8e372";
-const String PLACEHOLDER_IMAGE_URL = "https://developers.elementor.com/docs/assets/img/elementor-placeholder-image.png";
-const String GEMINI_API_KEY = "AIzaSyBvFqDb6M2tXmRpCey9Lyb3vKl32ht_PsA";
